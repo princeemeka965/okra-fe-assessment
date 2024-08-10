9a5baeebd8552926efc66e143ff7f264701167df
princeemeka965 <anyanwue4@gmail.com>
2024-08-11 00:35:53 +0100

    make adjustments to button component

--- src/components/Button.js
+++ src/components/Button.js
@@ -4,7 +4,7 @@ function Button(props) {
     return <>
         <button className={`flex justify-center gap-2 opacity-100 hover:opacity-50 ${props.class} ${props.disabled ? null : 'cursor-pointer'}`}>
             {props.disabled ? <span className="flex flex-col justify-center pt-1"><Spinner className="animate-spin" /></span> : null}
-            <span className={`${props.disabled ? 'text-surfaceSubdued' : 'text-primaryCTA'} text-sm mt-[1px]`}>{props.value}</span>
+            <span className={`${props.disabled ? 'text-surfaceSubdued' : `${props.textClass}`} text-sm mt-[1px]`}>{props.value}</span>
         </button>
     </>
 };
