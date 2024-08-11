4c52aa0dca7bf101e12b4c88537eea1716a779ad
princeemeka965 <anyanwue4@gmail.com>
2024-08-12 00:53:47 +0100

    modifications to the button component

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,8 +1,11 @@
 import { Spinner } from "./SvgIcons"
 
 function Button(props) {
+    const handleClick = () => {
+        props.onClick(true)
+    }
     return <>
-        <button className={`flex justify-center gap-2 opacity-100 hover:opacity-50 ${props.class} ${props.disabled ? null : 'cursor-pointer'}`}>
+        <button className={`flex justify-center gap-2 opacity-100 hover:opacity-50 ${props.class} ${props.disabled ? null : 'cursor-pointer'}`} onClick={handleClick}>
             {props.disabled ? <span className="flex flex-col justify-center pt-1"><Spinner className="animate-spin" /></span> : null}
             <span className={`${props.disabled ? 'text-surfaceSubdued' : `${props.textClass}`} text-sm mt-[1px]`}>{props.value}</span>
         </button>
