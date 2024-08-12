f210ec3eb519fc1e64ba4eb76defc1f9b1cca55f
princeemeka965 <anyanwue4@gmail.com>
2024-08-12 01:31:10 +0100

    set error handling for input component

--- src/components/Input.js
+++ src/components/Input.js
@@ -1,8 +1,8 @@
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 function Input(props) {
+    const inputRef = useRef(null);
     const [isFocused, setIsFocused] = useState(false);
-    const [hasError] = useState(props.error);
     const [inputValue, setInputValue] = useState(props.value);
 
     const handleFocus = () => {
@@ -20,10 +20,23 @@ function Input(props) {
         props.onChange(event.target.value)
     }
 
+    useEffect(() => {
+        // This effect runs whenever the `props.error` value changes.
+        // If `props.error` is true (i.e., there's an error),
+        // focus the input field using `inputRef.current.focus()`
+        // and also set the `isFocused` state to true, which is used to
+        // apply additional styles when the input is focused.
+        if (props.error) {
+            inputRef.current.focus();
+            setIsFocused(true);
+        }
+    }, [props.error]);
+
+
     return (
         <>
             <div className="w-full flex flex-col gap-2">
-                <div className={`w-full rounded-md flex flex-col h-[58px] gap-1 py-[10px] px-4 bg-surfaceSecondary focus-within:bg-surfaceWhite focus-within:border-[2px] ${hasError ? 'error focus-within:border-borderError' : 'input focus-within:border-borderPrimary'}`}
+                <div className={`w-full rounded-md flex flex-col h-[58px] gap-1 py-[10px] px-4 bg-surfaceSecondary focus-within:bg-surfaceWhite focus-within:border-[2px] ${props.error ? 'error focus-within:border-borderError' : 'input focus-within:border-borderPrimary'}`}
                 >
                     <span className="text-[10px] text-surfaceSubdued">{isFocused ? props.label : ''}</span>
                     <input type={props.type} placeholder={isFocused ? props.placeholder : props.label} value={inputValue} className={`w-full
@@ -31,9 +44,12 @@ function Input(props) {
                         onFocus={handleFocus}
                         onBlur={handleBlur}
                         onChange={handleChange}
-                        autoComplete="nope"
+                        autoComplete="off"
+                        ref={inputRef}
+                        name={props.name}
                     />
                 </div>
+                {props.error ? <p className="text-xs text-borderError">{props.error}</p> : null}
             </div>
         </>
     )
