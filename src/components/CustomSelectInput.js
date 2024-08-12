c58f84f6200c93ebc7c0437ec1a150a29ed611cd
princeemeka965 <anyanwue4@gmail.com>
2024-08-12 03:16:43 +0100

    fix React import

--- src/components/CustomSelectInput.js
+++ src/components/CustomSelectInput.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import React, { useState } from 'react';
 
 const CustomSelectInput = React.forwardRef((props, ref) => {
     const [selectedOption, setSelectedOption] = useState(props.placeholder || '');
